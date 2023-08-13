0a5c327ca5cdd1aabf5099ab8da1f6bc22be2b70
jinesh77 <jineshasokan77@gmail.com>
2023-08-13 16:32:25 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -36,8 +36,8 @@ window.addEventListener("scroll", () => {
 const navLinks = document.querySelectorAll(".nav-link");
 
 navLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    navLinks.forEach(link => link.classList.remove("active"));
-    link.classList.add("active");
-  });
+	link.addEventListener("click", () => {
+		navLinks.forEach(link => link.classList.remove("active"));
+		link.classList.add("active");
+	});
 });
